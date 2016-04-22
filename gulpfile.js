eef0df04b961240791188ccff5084197ea218a6c
shota <shota@shota-no-iMac.local>
2016-04-22 19:57:13 +0900

    ディレクトリ構成修正

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,26 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 
 gulp.task('compass', function(){
-    gulp.src('sass/**.sass').pipe(compass({
+    gulp.src('sass/*.sass').pipe(compass({
         config_file: 'config.rb',
         comments: false,
-        css: 'css/',
+        css: './',
         sass: 'sass/'
     }));
 });
 
 gulp.task('cssmin', function () {
-    gulp.src('css/main.css')
+    gulp.src('grid.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('sass/**.sass', function(event) {
+    gulp.watch('sass/*.sass', function(event) {
         gulp.run('compass');
     });
-    gulp.watch('css/**.css', function(event) {
+    gulp.watch('*.css', function(event) {
         gulp.run('cssmin');
     });
 });
